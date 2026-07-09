acd57ae236d641574801c22a12b08b19eea6e746
balakumar-r <balakumar.ramasankar@cognizant.com>
2026-07-09 12:02:53 +0530

    Added Global styles and variables with Footer fixes

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const {
+  src, dest, watch, series, parallel,
+} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
+const plumber = require('gulp-plumber');
+const browserSync = require('browser-sync').create();
+
+// Compile each component SCSS (exclude partials starting with _) and output CSS next to the SCSS
+function styles() {
+  return src('blocks/**/[^_]*.scss', { base: 'blocks', sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.')) // writes .map file next to .css
+    .pipe(dest('blocks')) // preserves folder structure because base is 'blocks'
+    .pipe(browserSync.stream({ match: '**/*.css' })); // inject CSS into browser
+}
+
+// Compile global SCSS in styles/ (exclude partials starting with _) into styles/*.css
+function globalStyles() {
+  return src('styles/[^_]*.scss', { base: 'styles', sourcemaps: true })
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+    .pipe(sourcemaps.write('.')) // writes .map file next to .css
+    .pipe(dest('styles'))
+    .pipe(browserSync.stream({ match: '**/*.css' }));
+}
+
+const buildStyles = parallel(styles, globalStyles);
+
+function serve() {
+  // If you run `aem up` and it serves on http://localhost:3000 use proxy. Otherwise set BS_PROXY env to something else.
+  const proxyTarget = process.env.BS_PROXY || 'http://localhost:3000';
+
+  // If you don't want to proxy (serve static files), comment proxy and use server option:
+  // browserSync.init({ server: { baseDir: './' }, open: false, notify: false });
+
+  browserSync.init({
+    proxy: proxyTarget,
+    open: false,
+    notify: false,
+  });
+
+  // Watch SCSS (including partials). When partials change we still run the styles task so dependent CSS updates.
+  watch('blocks/**/*.scss', styles);
+  watch('styles/**/*.scss', globalStyles);
+
+  // Watch other files (html, markup, etc.) and reload full page on changes
+  watch(['blocks/**/*.html', '*.html', 'index.html']).on('change', browserSync.reload);
+}
+
+exports.styles = buildStyles;
+exports.serve = series(buildStyles, serve);
+exports.watch = exports.serve;
+exports.default = exports.serve;
