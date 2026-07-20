aec54ea4757d296dade18e451be1608be57a1832
dinesh <dinesh.s214d72@cognizant.com>
2026-07-20 23:56:01 +0530

    columns-v1 update

---
+++ blocks/columns-v1/columns-v1.js
@@ -0,0 +1,18 @@
+export default function decorate(block) {
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((col) => {
+        // tag column containing an image
+        const pic = col.querySelector('picture');
+        if (pic) {
+          col.classList.add('columns-img-col');
+        }
+   
+        // tag the last paragraph if it contains a link (CTA)
+        const paragraphs = col.querySelectorAll('p');
+        const lastPara = paragraphs[paragraphs.length - 1];
+        if (lastPara && lastPara.querySelector('a')) {
+          lastPara.classList.add('columns-cta');
+        }
+      });
+    });
+  }
