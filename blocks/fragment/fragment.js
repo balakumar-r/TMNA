8253745e218c658d022d76a59d33d08a0bad5fba
balakumar-r <balakumar.ramasankar@cognizant.com>
2026-08-03 18:58:53 +0530

    Improve URL handling in resetAttributeBase function
    Enhance error handling for URL resetting in media attributes.

--- blocks/fragment/fragment.js
+++ blocks/fragment/fragment.js
@@ -27,10 +27,21 @@ export async function loadFragment(path) {
 
       // reset base path for media to fragment base
       const resetAttributeBase = (tag, attr) => {
-        main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
-          elem[attr] = new URL(elem.getAttribute(attr), new URL(path, window.location)).href;
-        });
-      };
+  main.querySelectorAll(`${tag}[${attr}^="./media_"]`).forEach((elem) => {
+    const value = elem.getAttribute(attr);
+
+    if (!value) return;
+
+    try {
+      elem[attr] = new URL(
+        value,
+        new URL(path, window.location.href),
+      ).href;
+    } catch (e) {
+      console.error('Invalid URL:', value, path);
+    }
+  });
+};
       resetAttributeBase('img', 'src');
       resetAttributeBase('source', 'srcset');
 
